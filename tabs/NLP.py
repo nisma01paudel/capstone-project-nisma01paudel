c03cd9a041433cd2f57fa07c7830073a045b15ab
nisma01paudel <npaudel048@gmail.com>
2025-05-14 17:40:45 +0545

    init

--- tabs/NLP.py
+++
@@ -1,9 +0,0 @@
-# tabs/NLP.py
-
-import streamlit as st
-
-def display():
-    st.subheader("🧠 NLP Analysis")
-    st.markdown("""
-        This section can be used to perform sentiment analysis or text classification tasks.
-    """)
